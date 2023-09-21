7292e9034b9312d2336828c057e271c3d509a5a2
shanestanley33 <shanestanleyah33@gmail.com>
2023-09-21 00:09:36 -0700

    Pseudocode_complete

---
+++ project.js
@@ -0,0 +1,14 @@
+//display start game screen img and start game button
+// get all the card flip buttons
+// get win message div
+// get game over screen display div
+// start game with all cards facing down/blacked out,
+// and game screen background displayed
+// attach click event for each a card
+// console.log(countmatches);
+// start timer on first card click
+// make sure cards dissappear when correctly matched
+// increase score by 1 for each match
+// when all cards are matched, display win message div
+// but if timer runs out first, display game over div
+// display replay or retry prompt and 'thanks for playing' on screen
