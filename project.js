6be15b85ea492ffda0900595993f9c544967246d
shanestanley33 <shanestanleyah33@gmail.com>
2023-09-28 10:29:56 -0700

    added readme file and trimmed unused code blubber

--- project.js
+++ project.js
@@ -37,57 +37,48 @@ let cardsMatched;
 let countdown;
 
 // Cached Elements
+
 //get start button by id and assign to a variable called startButton
 const startButton = document.getElementById(START_BUTTON_ID);
 //get game board by id and assign to a variable called gameBoard
 const gameBoard = document.getElementById(GAME_BOARD_ID);
 //get the score by id and (You need to make a div with id of score)
-//const scoreDisplay = document.getElementById(SCORE_DISPLAY_ID);
 const scoreDisplay = document.getElementById(SCORE_DISPLAY_ID);
 const winDisplay = document.getElementById('win-game')
-// const restartButton = document.getElementById(RESTART_BUTTON_ID);
 
 //Event Listeners
+
 //attach click event listener to startButton and called initialize function 
 startButton.addEventListener("click", initializeGame);
 gameBoard.addEventListener("click", handleCardClick);
-// restartButton.addEventListener("click", restartScreen);
-
-//console.log(K)
 
 // clicking a card
 function handleCardClick(event){
    console.log(event.target)
-  
    if (event.target.classList.contains('clicked')) {
       return
    }
    cardsFlipped.push(event.target)
-   
    if (cardsFlipped.length === 2){
        checkmatched();
    }
-   event.target.classList.add('clicked')   
-
+   event.target.classList.add('clicked')
    checkWinner();
 }
-
 function checkmatched() {
    if (cardsFlipped[0].innerText === cardsFlipped[1].innerText) {
-   // cardsMatched++;
       score++;
       cardsFlipped.forEach(card => {
-         console.log(card)
-         card.style.backgroundColor = 'green';
+      console.log(card)
+      card.style.backgroundColor = 'green';
       });
       console.log('its a match')
-      //disable those card from being used again?
    } else {
    console.log('no match')
    }
    let clickedEls = document.querySelectorAll('.clicked')
    clickedEls.forEach(el => {
-      el.classList.remove('clicked')
+   el.classList.remove('clicked')
    })
    cardsFlipped = []
    render()
@@ -105,6 +96,7 @@ function checkWinner() {
 }
 
 // Functions
+
 //define initialize function 
 function initializeGame(){
 score = INITIAL_SCORE;
@@ -141,14 +133,8 @@ function shuffleArray(array) {
 
 //define render function
 function render(){
-   console.log("innerText")
-   // Set the score display to the current score
-   
+   console.log("innerText")   
    scoreDisplay.innerText = `Score: ${score}`; 
-
-   // Create card 
-
-   //createCards();
 };
 
 
@@ -178,39 +164,8 @@ function resetEverything() {
    removeCard();
 }
 
-// function movesCounter() {
-//    // Update the html for the moves counter
-//    movesCount.innerHTML++;
-//    // Keep track of the number of moves for every pair checked
-//    moves++;
-//  }
-
-
-//define a renderCards function 
-//loop over the constants card array 
-//and for each card, create a new div element 
-//set the innerText of the div element to the value of the element in the array 
-//attach an on-click event listener to the element, define a callback function 
-//append the element to the gameBoard 
-
-//onCardClick()
-//check if there are two elements already in the flippedCards array
-//if so, compare the values
-//if not, add the element 
-
-
-
-// display start game screen img and start game button
-// get all the card flip buttons
-// get win message div
-// get game-over screen display div
 // start game with all cards facing down/blacked out,
-// and game screen background displayed
-// attach click event for each card
-// console.log(countmatches);
-// (do this last) start timer on first card click
+//(do this last) start timer on first card click
 // make sure cards dissappear when correctly matched
-// increase score by 1 for each match
-// if all cards are matched, display win message div
 // else if timer runs out first, display game over div
 // and display replay or retry prompt and 'thanks for playing' on screen
---
+++ readme/README.md
@@ -0,0 +1,20 @@
+## 🃏 Chemistry Card Flipper
+
+**[Card Flipper](https://shanestanley33.github.io/project1/) is an intuitive game for all ages. This is a take on the classic Concentration-style game.**
+
+## ❓ How to play?
+**You are provided with a set of cards with images on their faces. At the start of the game, the cards are turned face down. Click on a card and it will turn over to reveal the image. Find two cards with the same image to make a match. Once you have found all of the matches, you win the game!**
+
+## 📷 Screenshots.
+<p align= "center"><img src="/Users/shanestanley/code/school_projects/project1/readme/Screenshot 2023-09-28 at 10.15.12 AM.png"></p>
+
+## 🔈 Languages Used.
+<img align="center" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="CSS" height="50" width="67"/><img align="center" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="HTML" height="50" width="67"/><img align="center" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="CSS" height="50" width="67"/>
+
+## 🔗 Link to game : [Chemistry Card Flip Game](https://shanestanley33.github.io/project1/) 
+
+## Future plans: 
+1. highlight and un-highlight cards that are in use
+2. @media screen and (min-width) for different screen sizes
+3. add a countdown timer and game-over if time runs out
+4. add a pause/stop music button
